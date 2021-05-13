04caed1b3b32ced65747d94664bc9147d0be203f
Srevin Saju <srevinsaju@sugarlabs.org>
2021-05-13 11:47:33 +0300

    feat: do not install node

--- scripts/centos/7/appimage/02-create_appimage.sh
+++ scripts/centos/7/appimage/02-create_appimage.sh
@@ -50,15 +50,15 @@ cd "$RT/_deps"
 
 cd "$RT"
 
-curl --silent --location https://rpm.nodesource.com/setup_$NODE_VERSION | sudo bash -
-sudo yum -y install nodejs
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
-sudo yum -y install yarn
-yarn --version
-node --version
-sudo yarn global add neon-cli
-neon version
+#curl --silent --location https://rpm.nodesource.com/setup_$NODE_VERSION | sudo bash -
+#sudo yum -y install nodejs
+#curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+#sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+#sudo yum -y install yarn
+#yarn --version
+#node --version
+#sudo yarn global add neon-cli
+#neon version
 
 
 cd "$RT"
