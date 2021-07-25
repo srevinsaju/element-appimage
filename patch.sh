1c1d77d81846e0967e9952829d4157d4e35bfbba
Srevin Saju <srevinsaju@sugarlabs.org>
2021-07-25 19:01:18 +0300

    fix: attempt to do so.com/q/40429927

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 
 
 set -eux
-sed -i 's,import AutoLaunch from "auto-launch";,import AutoLaunch from "auto-launch";import appimage from "./appimage";,g' src/electron-main.ts
+sed -i 's,import AutoLaunch from "auto-launch";,import AutoLaunch from "auto-launch";import * as appimage from "./appimage";,g' src/electron-main.ts
 sed -i "s,launcher.enable();,appimage.enableAutoStart();,g" src/electron-main.ts
 sed -i "s,launcher.disable(),appimage.disableAutoStart(),g" src/electron-main.ts
 sed -i "s,await launcher.isEnabled(),appimage.isAutoStartEnabled(),g" src/electron-main.ts
