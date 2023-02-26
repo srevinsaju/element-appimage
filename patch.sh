9f4a81b5c9503f96b4d86b76155f826ec7e1ec29
Srevin Saju <srevinsaju@sugarlabs.org>
2023-02-26 20:56:48 +0530

    feat: update patch script

--- patch.sh
+++ patch.sh
@@ -3,6 +3,6 @@
 
 set -eux
 sed -i 's,import AutoLaunch from "auto-launch";,import AutoLaunch from "auto-launch";import * as appimage from "./appimage";,g' src/electron-main.ts
-sed -i "s,launcher.enable();,appimage.enableAutoStart();,g" src/electron-main.ts
-sed -i "s,launcher.disable(),appimage.disableAutoStart(),g" src/electron-main.ts
-sed -i "s,await launcher.isEnabled(),appimage.isAutoStartEnabled(),g" src/electron-main.ts
+sed -i "s,global.launcher.enable();,appimage.enableAutoStart();,g" src/electron-main.ts
+sed -i "s,global.launcher.disable(),appimage.disableAutoStart(),g" src/electron-main.ts
+sed -i "s,await oldLauncher.isEnabled(),appimage.isAutoStartEnabled(),g" src/electron-main.ts
